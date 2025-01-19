79ee8cc49647b1155c02dd1f0da0a4a11ee796e6
strwalk <61673527+strwalk@users.noreply.github.com>
2025-01-19 13:50:51 +0900

    feat: add basic views file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Gourmet
 
 # Create your views here.
-def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+class ReviewList(ListView):
+    model = Gourmet
+    fields = '__all__'
+    template_name = 'app/index.html'
+
+class ReviewCreate(CreateView):
+    model = Gourmet
+    fields = ['restaurant_name', 'memo', 'rating']
+    template_name = 'app/create.html'
+    success_url = reverse_lazy('list')
+
+class ReviewUpdate(UpdateView):
+    model = Gourmet
+    fields = ['restaurant_name', 'memo', 'rating']
+    template_name = 'app/update.html'
+    success_url = reverse_lazy('list')
+
+class ReviewDelete(DeleteView):
+    model = Gourmet
+    template_name = 'app/delete.html'
+    success_url = reverse_lazy('list')
