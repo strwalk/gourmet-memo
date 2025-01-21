8324d0357889c9652cf1d5247b9830cd89e70370
strwalk <61673527+strwalk@users.noreply.github.com>
2025-01-21 23:40:15 +0900

    feat: add login and logout

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ReviewList, ReviewCreate, ReviewUpdate, ReviewDelete
+from .views import ReviewList, ReviewCreate, ReviewUpdate, ReviewDelete, UserLogin, UserLogout
 
 urlpatterns = [
     path('', ReviewList.as_view(), name='list'),
     path('create/', ReviewCreate.as_view(), name='create'),
     path('update/<int:pk>', ReviewUpdate.as_view(), name='update'),
-    path('delete/<int:pk>', ReviewDelete.as_view(), name='delete')
+    path('delete/<int:pk>', ReviewDelete.as_view(), name='delete'),
+    path('login/', UserLogin.as_view(), name='login'),
+    path('logout/', UserLogout.as_view(), name='logout')
 ]
--- app/views.py
+++ app/views.py
@@ -1,26 +1,45 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Gourmet
 
-# Create your views here.
-class ReviewList(ListView):
+class ReviewList(LoginRequiredMixin, ListView):
     model = Gourmet
     fields = '__all__'
     template_name = 'app/index.html'
+    login_url = reverse_lazy('login')
 
-class ReviewCreate(CreateView):
+class ReviewCreate(LoginRequiredMixin, CreateView):
     model = Gourmet
     fields = ['restaurant_name', 'memo', 'rating']
     template_name = 'app/create.html'
+    login_url = reverse_lazy('login')
     success_url = reverse_lazy('list')
 
-class ReviewUpdate(UpdateView):
+class ReviewUpdate(LoginRequiredMixin, UpdateView):
     model = Gourmet
     fields = ['restaurant_name', 'memo', 'rating']
     template_name = 'app/update.html'
+    login_url = reverse_lazy('login')
     success_url = reverse_lazy('list')
 
-class ReviewDelete(DeleteView):
+class ReviewDelete(LoginRequiredMixin, DeleteView):
     model = Gourmet
     template_name = 'app/delete.html'
+    login_url = reverse_lazy('login')
     success_url = reverse_lazy('list')
+
+class UserLogin(LoginView):
+    template_name = 'app/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('list')
+
+class UserLogout(LogoutView):
+    def get_success_url(self):
+        return reverse_lazy('login')
+
